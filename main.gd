4d3a6540bb09f80730551550199fee39b75bbe49
Chelsea <ch538553@ucf.edu>
2026-04-06 21:28:58 -0400

    Successfully updates user data

--- main.gd
+++ main.gd
@@ -2,11 +2,14 @@ extends Node2D
 
 
 func _ready()->void:
-	pass
+	_connect_test()
 
 func _connect_test() -> void:
 	Firebase.Auth.login_anonymous() # will have to replace with actual auth
 	var collection: FirestoreCollection = Firebase.Firestore.collection('Users') # connect to specific collection
 	#var document = await collection.add("", {'name': 'test', 'active': 'true'})
-	var get = await collection.get_doc('test') # example of a get
-	print(get)
+	var document = await collection.get_doc('l7h92PD7t2k9r8KbayAp') # example of a get
+	print(document)
+	
+	document["displayname"] = "JoelIsCoolest" # example of an update
+	var update = await collection.update(document) 
