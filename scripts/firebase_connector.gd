640ebec41f025a939c4442491c1f9d31c93ecc55
ckfrancois <francois.chelseak@gmail.com>
2026-04-16 14:12:03 -0400

    Imported Buttons and Label Text Boxes

--- scripts/firebase_connector.gd
+++ scripts/firebase_connector.gd
@@ -1,14 +1,15 @@
 extends Node
 
 
-@export var confirm_button:Button = null
+@export var confirm_button:TextureButton = null
 
 @export var introduction_label:Label = null
 
 func _ready()->void:
-	confirm_button.connect("pressed", AuthManager.set_passling_data)
-	AuthManager.got_display_name.connect(_write_introduction_label)
-	introduction_label.text = "Hi! I'm \n"
+	pass
+	#confirm_button.connect("pressed", AuthManager.set_passling_data)
+	#AuthManager.got_display_name.connect(_write_introduction_label)
+	#introduction_label.text = "Hi! I'm \n"
 
 func _write_introduction_label(display_name:String) -> void:
 	introduction_label.text = "Hi! I'm\n" + display_name + ""
